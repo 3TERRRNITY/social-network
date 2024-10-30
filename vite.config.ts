f6f75ccb97c02612824cc875728a96440a1c52ae
3TERRRNITY <3terrrnity@gmail.com>
2024-10-30 15:58:05 +0300

    finilize project

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,10 +7,8 @@ export default defineConfig({
   server: {
     open: true,
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
+  build: {
+    outDir: 'build',
+    sourcemap: true,
   },
 })
